1b3336b7e053178d78c5261c33cb8f1ef8b4de7e
test <sikveh@gmail.com>
2019-11-02 21:35:34 +0300

    GitTest

--- GitTest/GitTest/Program.cs
+++ GitTest/GitTest/Program.cs
@@ -10,7 +10,7 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world!!!");
         }
     }
 }
